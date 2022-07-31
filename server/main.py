c551881500703d7241a416e60ff57ce1fbd302e3
Sourcery AI <>
2022-07-31 15:33:22 +0000

    'Refactored by Sourcery'

--- server/main.py
+++ server/main.py
@@ -27,9 +27,6 @@ async def root():
     return {'msg": "API is Online'}
 
 
-if __name__ == "__main__":
-    if os.environ.get('APP_ENV') == "development":
-        uvicorn.run("main:api", host="0.0.0.0", port=3500,
-                    workers=4, reload=True)
-    else:
-        pass
+if __name__ == "__main__" and os.environ.get('APP_ENV') == "development":
+    uvicorn.run("main:api", host="0.0.0.0", port=3500,
+                workers=4, reload=True)
